40a1494dee3719ddbf81660735052547518bfe81
ChengZhi <chengzhi219@dingtalk.com>
2021-05-27 08:59:59 +0800

    21.5.27

--- 实验三/7-6.asm
+++ 实验三/7-6.asm
@@ -1,1 +1,13 @@
 data segment
+  infor0 db,0ah,0dh,'sort=$'
+  infor1 db,0ah,0dh,'input name:$'
+  infor2 db,0ah,0dh,'input score:$'
+  n equ 8
+  m equ 4
+  p equ 3
+  q equ 3
+  buff1 db n,?,n+1 dup('$')
+  buff2 db m,?,m+1 dup('$')
+  sname db p dup(n+1 dup('$'))
+  score1 dw p dup(m+1 dup('$'))
+  score2 dw p dup(m+1 dup(0))
